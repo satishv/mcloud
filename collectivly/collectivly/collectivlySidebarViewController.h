65ba3026e82d5dfb76a18c4e6748b4c944541cdc
Nathan Fraenkel <fraenkel@seas.upenn.edu>
2013-05-27 14:30:14 -0700

    started refining side view

---
+++ collectivly/collectivly/collectivlySidebarViewController.h
@@ -0,0 +1,13 @@
+//
+//  collectivlySidebarViewController.h
+//  collectivly
+//
+//  Created by Nathan Fraenkel on 5/27/13.
+//  Copyright (c) 2013 Nathan Fraenkel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface collectivlySidebarViewController : UIViewController
+
+@end
---
+++ collectivly/collectivly/collectivlySidebarViewController.m
@@ -0,0 +1,38 @@
+//
+//  collectivlySidebarViewController.m
+//  collectivly
+//
+//  Created by Nathan Fraenkel on 5/27/13.
+//  Copyright (c) 2013 Nathan Fraenkel. All rights reserved.
+//
+
+#import "collectivlySidebarViewController.h"
+
+@interface collectivlySidebarViewController ()
+
+@end
+
+@implementation collectivlySidebarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
